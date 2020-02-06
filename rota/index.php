1df508c213af78b21d9566bb934ed36da9c78b7e
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-02-06 09:13:34 -0300

    typewriter javascript

--- rota/index.php
+++ rota/index.php
@@ -12,6 +12,7 @@ $nome = 'Matheus Ricardo Brunelli';
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= 'Dia: ' . date('d/m/Y') . ' ' . date('H:i:s')   ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
 </head>
 
 <body>
@@ -26,17 +27,25 @@ $nome = 'Matheus Ricardo Brunelli';
     <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
     <script>
         function abreModal(conteudo) {
             $.ajax({
+                // SE EU TIVER MAIS DE UM MODAL EU PRECISO PASSAR UM GET NA URL 
                 url: 'modal.php?acao=texto',
                 type: 'post',
                 data: {
-                    conteudo
+                    conteudo,
+                    'idade': 19
                 },
+                // CALL BACK 
                 success: function(result) {
                     console.log(result, conteudo)
+                    $(result).modal('show')
+                },
+                error: function(result) {
+                    console.log(result.status)
                 }
             })
         }
